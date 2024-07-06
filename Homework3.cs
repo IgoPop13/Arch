046992b2c1c65ca9c8c1df1ac36a8badc0b5c6b7
IgoPop13 <igor-popov@yandex.ru>
2024-07-06 22:04:34 +0300

    Update Homework3.cs

--- Homework3.cs
+++ Homework3.cs
@@ -6,7 +6,7 @@
 // Описание/Пошаговая инструкция выполнения домашнего задания:
 // Предположим, что все команды находятся в некоторой очереди. Обработка очереди заключается в чтении очередной команды и головы очереди и вызова метода Execute извлеченной команды. Метод Execute() может выбросить любое произвольное исключение.
 
-// Обернуть вызов Команды в блок try-catch.
+// OK Обернуть вызов Команды в блок try-catch.
 // Обработчик catch должен перехватывать только самое базовое исключение.
 // Есть множество различных обработчиков исключений. Выбор подходящего обработчика исключения делается на основе экземпляра перехваченного исключения и команды, которая выбросила исключение.
 // Реализовать Команду, которая записывает информацию о выброшенном исключении в лог.
@@ -31,9 +31,184 @@
 
 
 using System;
+using System.Collections;
 
-namespace HomeWorkTwo
+namespace HomeWorkThree
 {
+    class RunTest
+    {
+        void Exec()
+        {
+            Rotate rotate;
+            Move move;
+            rotate = new Rotate (new Rotable (new Angle(0, 16), 3));
+
+            Queue<ICommand> q = new Queue();
+
+            // correct
+            move = new Move (new Movable(new Vector(12, 5), new Vector(-7, 3)));
+            q.Enqueue(move);
+            
+            // exceptions
+            move = new Move (new MovableNoLocation(new Vector(12, 5), new Vector(-7, 3)));
+            q.Enqueue(move);
+            move = new Move (new MovableNoVelocity(new Vector(12, 5), new Vector(-7, 3)));
+            q.Enqueue(move);
+            move = new Move (new MovableCantMove(new Vector(12, 5), new Vector(-7, 3)));
+            q.Enqueue(move);
+
+            foreach(ICommand c in q)
+            {
+                try
+                {
+                    c.Execute();
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+
+    class Log : ICommand
+    {
+        public Log(ICommand c, Exception e)
+        {
+        }
+        public void Execute()
+        {
+            // Write to log
+        }
+    }
+
+    interface IRotable
+    {
+        public Angle GetAngle();
+        public int GetAngularVelocity();
+        public void SetAngle(Angle newValue);
+    }
+
+    class Rotate
+    {
+        IRotable _rotable;
+        public Rotate(IRotable rotable)
+        {
+            _rotable = rotable;
+        }
+        public void  Execute()
+        {
+            _rotable.SetAngle(_rotable.GetAngle().Plus(_rotable.GetAngularVelocity()));
+        }
+    }
+
+    class Rotable : IRotable
+    {
+        private Angle _angle;
+        private int _angularVelocity;
+        public Rotable(Angle angle, int angularVelocity)
+        {
+            _angle = angle;
+            _angularVelocity = angularVelocity;
+        }
+        public Angle GetAngle()
+        {
+            return _angle;
+        }
+        public int GetAngularVelocity()
+        {
+            return _angularVelocity;
+        }
+        public void SetAngle(Angle angle)
+        {
+            _angle = angle;
+        }
+    }
+
+    interface IMovable
+    {
+        public Vector GetLocation();
+        public Vector GetVelocity();
+        public void SetLocation(Vector newValue);
+    }
+
+    interface ICommand
+    {
+        public void Execute();
+    }
+    
+    class Move : ICommand
+    {
+        IMovable _movable;
+        public Move(IMovable movable)
+        {
+            _movable = movable;
+        }
+        public void  Execute()
+        {
+            _movable.SetLocation(Vector.Plus(_movable.GetLocation(), _movable.GetVelocity()));
+        }
+    }
+
+    class Movable : IMovable 
+    {
+        private Vector _location;
+        private Vector _velocity;
+        public Movable(Vector location, Vector velocity)
+        {
+            _location = location;
+            _velocity = velocity;
+        }
+        public Vector GetLocation() { return _location; }
+        public Vector GetVelocity() { return _velocity; }
+        public void SetLocation(Vector location) { _location = location; }
+    }
+
+    class NoLocationException : Exception {}
+    class NoVelocityException : Exception {}
+    class NoMovementException : Exception {}
+
+    // MOCs
+    class MovableNoVelocity : IMovable // MOC
+    {
+        private Vector _location;
+        private Vector _velocity;
+        public MovableNoVelocity(Vector location, Vector velocity)
+        {
+            _location = location;
+            _velocity = velocity;
+        }
+        public Vector GetLocation() { return _location; }
+        public Vector GetVelocity() { throw new NoVelocityException(); } // "Can't get velocity."
+        public void SetLocation(Vector location) { _location = location; }
+    }
+
+    class MovableCantMove : IMovable  // MOC
+    {
+        private Vector _location;
+        private Vector _velocity;
+        public MovableCantMove(Vector location, Vector velocity)
+        {
+            _location = location;
+            _velocity = velocity;
+        }
+        public Vector GetLocation() { return _location; }
+        public Vector GetVelocity() { return _velocity; }
+        public void SetLocation(Vector location) { throw new NoMovementException(); } // "Can't move."
+    }
+
+    class MovableNoLocation : IMovable // MOC
+    {
+        private Vector _location;
+        private Vector _velocity;
+        public MovableNoLocation(Vector location, Vector velocity)
+        {
+            _location = location;
+            _velocity = velocity;
+        }
+        public Vector GetLocation() { throw new NoLocationException(); } // "Can't get location."
+        public Vector GetVelocity() { return _velocity; }
+        public void SetLocation(Vector location) { _location = location; }
+    }
 
     class Vector
     {
@@ -79,151 +254,6 @@ namespace HomeWorkTwo
         public int GetAngle()
         {
             return _angle;
-        }
-    }
-    
-    interface IRotable
-    {
-        public Angle GetAngle();
-        public int GetAngularVelocity();
-        public void SetAngle(Angle newValue);
-    }
-
-    class Rotate
-    {
-        IRotable _rotable;
-        public Rotate(IRotable rotable)
-        {
-            _rotable = rotable;
-        }
-        public void  Execute()
-        {
-            _rotable.SetAngle(_rotable.GetAngle().Plus(_rotable.GetAngularVelocity()));
-        }
-    }
-
-    class Rotable : IRotable
-    {
-        private Angle _angle;
-        private int _angularVelocity;
-        public Rotable(Angle angle, int angularVelocity)
-        {
-            _angle = angle;
-            _angularVelocity = angularVelocity;
-        }
-        public Angle GetAngle()
-        {
-            return _angle;
-        }
-        public int GetAngularVelocity()
-        {
-            return _angularVelocity;
-        }
-        public void SetAngle(Angle angle)
-        {
-            _angle = angle;
-        }
-    }
-
-
-    interface IMovable
-    {
-        public Vector GetLocation();
-        public Vector GetVelocity();
-        public void SetLocation(Vector newValue);
-    }
-
-    class Move
-    {
-        IMovable _movable;
-        public Move(IMovable movable)
-        {
-            _movable = movable;
-        }
-        public void  Execute()
-        {
-            _movable.SetLocation(Vector.Plus(_movable.GetLocation(), _movable.GetVelocity()));
-        }
-    }
-
-    class Movable : IMovable 
-    {
-        private Vector _location;
-        private Vector _velocity;
-        public Movable(Vector location, Vector velocity)
-        {
-            _location = location;
-            _velocity = velocity;
-        }
-        public Vector GetLocation() { return _location; }
-        public Vector GetVelocity() { return _velocity; }
-        public void SetLocation(Vector location) { _location = location; }
-    }
-
-    class MovableNoLocation : IMovable // MOC
-    {
-        private Vector _location;
-        private Vector _velocity;
-        public MovableNoLocation(Vector location, Vector velocity)
-        {
-            _location = location;
-            _velocity = velocity;
-        }
-        public Vector GetLocation() { throw new NoLocationException(); } // "Can't get location."
-        public Vector GetVelocity() { return _velocity; }
-        public void SetLocation(Vector location) { _location = location; }
-    }
-
-    class MovableNoVelocity : IMovable
-    {
-        private Vector _location;
-        private Vector _velocity;
-        public MovableNoVelocity(Vector location, Vector velocity)
-        {
-            _location = location;
-            _velocity = velocity;
-        }
-        public Vector GetLocation() { return _location; }
-        public Vector GetVelocity() { throw new NoVelocityException(); } // "Can't get velocity."
-        public void SetLocation(Vector location) { _location = location; }
-    }
-
-    class MovableCantMove : IMovable  // MOC
-    {
-        private Vector _location;
-        private Vector _velocity;
-        public MovableCantMove(Vector location, Vector velocity)
-        {
-            _location = location;
-            _velocity = velocity;
-        }
-        public Vector GetLocation() { return _location; }
-        public Vector GetVelocity() { return _velocity; }
-        public void SetLocation(Vector location) { throw new NoMovementException(); } // "Can't move."
-    }
-    
-    class NoLocationException : Exception {}
-    class NoVelocityException : Exception {}
-    class NoMovementException : Exception {}
-
-    class RunTest
-    {
-        void Exec()
-        {
-            Rotate rotate;
-            Move move;
-            // correct
-            rotate = new Rotate (new Rotable (new Angle(0, 16), 3));
-            move = new Move (new Movable(new Vector(12, 5), new Vector(-7, 3)));
-            move.Execute();
-
-            // exceptions
-            move = new Move (new MovableNoLocation(new Vector(12, 5), new Vector(-7, 3)));
-            move.Execute();
-            move = new Move (new MovableNoVelocity(new Vector(12, 5), new Vector(-7, 3)));
-            move.Execute();
-            move = new Move (new MovableCantMove(new Vector(12, 5), new Vector(-7, 3)));
-            move.Execute();
         }
     }
 }
